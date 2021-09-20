1f4713b04d769bb30918aedef212c6bf5667e811
evellyinhelenoir <evellyin.nunes@zup.com.br>
2021-09-20 00:39:38 -0300

    Exercicio04

---
+++ src/br/com/zup/Modulo03_Lista1/Exercicio04/Carro.java
@@ -0,0 +1,17 @@
+package br.com.zup.Modulo03_Lista1.Exercicio04;
+
+//quantidade de portas, modelo, marca e potência.
+
+public class Carro {
+    int portas = 0;
+    double potencia = 2.0;
+    String modelo = "";
+    String marca = "";
+
+    public Carro (int portas, String modelo, String marca){
+        this.portas = portas;
+        this.modelo = modelo;
+        this.marca = marca;
+
+    }
+}
---
+++ src/br/com/zup/Modulo03_Lista1/Exercicio04/Main.java
@@ -0,0 +1,11 @@
+package br.com.zup.Modulo03_Lista1.Exercicio04;
+
+public class Main {
+    public static void main(String[] args) {
+        Carro carro = new Carro(4, "Corsa", "Chevrolet");
+        System.out.println(carro.portas);
+        System.out.println(carro.modelo);
+        System.out.println(carro.potencia);
+        System.out.println(carro.marca);
+    }
+}
